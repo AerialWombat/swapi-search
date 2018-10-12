147f97eaf51f8141e825f9d90cafbf880acafdd1
AerialWombat <adrianzhan@gmail.com>
2018-10-12 16:07:00 -0400

    Updates

---
+++ precache-manifest.2d14339529a146c21e23f4399d3dba12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeab111170ef246d8ec0985e9456155a",
+    "url": "/swapi-search/static/media/stars.eeab1111.jpg"
+  },
+  {
+    "revision": "8cff3622f4e1925ecb59",
+    "url": "/swapi-search/static/js/runtime~main.8cff3622.js"
+  },
+  {
+    "revision": "3b71f76cae653e077fbf",
+    "url": "/swapi-search/static/js/main.3b71f76c.chunk.js"
+  },
+  {
+    "revision": "71e7d1ec51193dbcbd71",
+    "url": "/swapi-search/static/js/1.71e7d1ec.chunk.js"
+  },
+  {
+    "revision": "3b71f76cae653e077fbf",
+    "url": "/swapi-search/static/css/main.892f4a4d.chunk.css"
+  },
+  {
+    "revision": "32c7d36eb7ea9eab54b62bfcc60aa78e",
+    "url": "/swapi-search/index.html"
+  }
+];
